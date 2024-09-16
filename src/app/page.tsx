bda252c4191657d3c62dbb0a84174e9423640154
vitek <toho0036@gmail.com>
2024-09-16 17:26:41 +0200

    styling and adding waiting list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen w-full">
       <h1 className="text-2xl font-bold mb-8">Registration Form</h1>
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSeExs5B-2MmyZ7vwZ5DlPqIXpNSD1fp6rszHAqlSimxnZUFzA/viewform?embedded=true"
-        width="100"
-        height="1010"
-        frameBorder="0"
-        marginHeight={0}
-        marginWidth={0}
-        className="w-full"
-        title="Registration Form"
-      >
-        Loading…
-      </iframe>
+      <div className="w-full px-4 max-w-7xl">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSeExs5B-2MmyZ7vwZ5DlPqIXpNSD1fp6rszHAqlSimxnZUFzA/viewform?embedded=true"
+          style={{ width: '100%', height: '800px' }}
+          frameBorder="0"
+          marginHeight={0}
+          marginWidth={0}
+          title="Registration Form"
+        >
+          Loading…
+        </iframe>
+      </div>
     </div>
   );
 }
--- src/app/waiting-list/page.tsx
+++ src/app/waiting-list/page.tsx
@@ -1,20 +1,43 @@
+'use client';
+import { useEffect, useRef } from 'react';
+
 export default function WaitingList() {
+  // Specify the ref type as HTMLIFrameElement or null
+  const sheetRef = useRef<HTMLIFrameElement>(null);
+
+  useEffect(() => {
+    // Set the iframe to reload every 60 seconds (60000 ms)
+    const intervalId = setInterval(() => {
+      if (sheetRef.current) {
+        sheetRef.current.src += ''; // Refreshes the iframe
+      }
+    }, 60000); // Adjust time as necessary
+
+    return () => clearInterval(intervalId); // Cleanup on unmount
+  }, []);
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen w-full">
+      <div className="w-full px-4 max-w-7xl flex justify-center">
+        <iframe
+          ref={sheetRef}
+          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRzrXH8yWpsFf-6OB9ZgHN7kbYkjA0ZjAac5thG5e9UmX66ZOdcWvU3BzcOr0xkTDba2xFgTBiEJ5QY/pubhtml?gid=1582545733&single=true"
+          style={{ width: '40%', height: '300px' }}
+        ></iframe>
+      </div>
       <h1 className="text-2xl font-bold mb-8">Waiting List Form</h1>
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSepbUYkdiBAQGNLdKxsBqHrkGOJUH-qnQmQjE5VsXlRh5nUXQ/viewform?embedded=true"
-        width="640"
-        height="1082"
-        frameBorder="0"
-        marginHeight={0}
-        marginWidth={0}
-        className="w-full max-w-lg"
-        title="Waiting List Form"
-      >
-        Loading…
-      </iframe>
-      ;
+      <div className="w-full px-4 max-w-7xl flex justify-center">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSepbUYkdiBAQGNLdKxsBqHrkGOJUH-qnQmQjE5VsXlRh5nUXQ/viewform?embedded=true"
+          style={{ width: '100%', height: '800px', maxWidth: '800px' }}
+          frameBorder="0"
+          marginHeight={0}
+          marginWidth={0}
+          title="Waiting List Form"
+        >
+          Loading…
+        </iframe>
+      </div>
     </div>
   );
 }
